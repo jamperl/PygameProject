343152bff55078c47098da7fe65ecf94b82d73f6
Jamie Perlmutter <jamieperlmutter@Jamies-MacBook-Pro.local>
2016-11-29 15:35:25 -0500

    Rival School Sprite Class Creation

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import *
 from pygame.sprite import *
+from pygame.locals import *
 import random
 
 WINDOW_WIDTH = 640
@@ -12,8 +13,8 @@ RED = (255, 0, 0)
 pygame.mixer.init()
 pygame.init()
 
-screen = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
-big_house = pygame.image.load("field.bmp")
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+big_house = pygame.image.load('field.bmp')
 pygame.transform.scale(big_house, (WINDOW_WIDTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption("Champions of the West")
@@ -25,7 +26,7 @@ class Jabrill(pygame.sprite.Sprite):
 		self.width = 80
 		self.height = 130
 
-		self.pic = pygame.image.load("jabrill.bmp")
+		self.pic = pygame.image.load('jabrill.bmp')
 		self.pic = pygame.transform.scale(self.pic, (self.width, self.height))
 
 		self.rect = self.pic.get_rect()
@@ -63,6 +64,7 @@ class Rival(pygame.sprite.Sprite):
 		self.width = 60
 		self.height = 95
 
+
 		self.school_pics = ['osu.bmp', 'msu.bmp']
 		self.new_school_pics = []
 		for pic in self.school_pics:
@@ -73,28 +75,40 @@ class Rival(pygame.sprite.Sprite):
 		self.pic = random.choice(self.new_school_pics)
 		self.rect = self.pic.get_rect()
 
+		self.rect.start = -20
+		self.rect.pos = random.randrange(0, (WINDOW_WIDTH - self.rect.WINDOW_WIDTH))
+		self.vert = random.randrange(3, 10)
 
+	def update(self):
+		self.rect.start += self.vert
+		if self.rect.top > WINDOW_HEIGHT - 5:
+			self.rect.start = -20
+			self.rect.pos = random.randrange((0 + self.rect.width), (WINDOW_WIDTH - self.rect.width))
+			self.vert = random.randrange(3, 10)
 
 
 sprites_list = pygame.sprite.Group()
 jabrill = Jabrill()
 sprites_list.add(jabrill)
 
-#schools, helmets = pygame.sprite.Group()
-# for x in range(8):
-# 	rival = Rival()
-# 	sprites_list.add(rival)
-# 	schools.add(rival)
+schools = pygame.sprit.Group()
+for x in range(8):
+	rival = Rival()
+	sprites_list.add(rival)
+	schools.add(rival)
+
+#helmets = pygame.sprite.Group()
+
 
 gameExit = False
 while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
-
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			gameExit = True
+	screen.blit(big_house, (0,0))
 	sprites_list.update()
+	sprites_list.draw(screen)
 
 	pygame.display.flip()
-
 pygame.quit()
 quit()
